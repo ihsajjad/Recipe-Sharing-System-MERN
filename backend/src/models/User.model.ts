802440bc70384f43c156d19349709fb25e40f190
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-05-25 16:45:08 +0600

    Backend: Created user model

---
+++ backend/src/models/User.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { UserDataType } from "../shared/types";
+
+const UserSchema = new mongoose.Schema<UserDataType>({
+  displayName: { type: String, required: true },
+  photoURL: { type: String },
+  email: { type: String, required: [true, "Email is required"], unique: true },
+  coins: { type: Number, required: true, default: 0 },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
---
+++ backend/src/shared/types.ts
@@ -0,0 +1,7 @@
+export type UserDataType = {
+  _id?: string;
+  displayName: string;
+  email: string;
+  photoURL: string;
+  coins: number;
+};
