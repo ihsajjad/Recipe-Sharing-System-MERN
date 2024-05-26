5246f549ce0b1e4f2f24deff6cd0285ee19ae065
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-05-26 11:06:31 +0600

    Frontend: completed the recipe card

---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,26 @@
+import { RecipeCardType } from "../../../backend/src/shared/types";
+
+const RecipeCard = ({ recipe }: { recipe: RecipeCardType }) => {
+  return (
+    <div className="border border-[var(--secondary-color)] p-4 bg-[var(--secondary-bg)] rounded space-y-3 shadow-lg shadow-slate-300 max-w-[400px] hover:bg-[var(--primary-bg)]">
+      <h3 className="text-xl font-bold text-[var(--secondary-color)]">
+        {recipe.recipeName}
+      </h3>
+      <img
+        src={recipe.recipeImage}
+        alt={recipe.recipeName}
+        className="object-cover h-44 w-full rounded"
+      />
+      <div className="flex flex-col">
+        <span>Country: {recipe.country}</span>
+        <span>Creator: {recipe.creatorEmail}</span>
+      </div>
+
+      <button className="cursor-pointer text-white px-3 py-1 font-semibold rounded bg-[var(--secondary-color)] duration-300 opacity-90 hover:opacity-100 mx-auto w-full">
+        View The Recipe
+      </button>
+    </div>
+  );
+};
+
+export default RecipeCard;
--- frontend/src/pages/AllRecipes.tsx
+++ frontend/src/pages/AllRecipes.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { RecipeCardType, SearchQuery } from "../../../backend/src/shared/types";
 import * as apiClient from "../api-client";
+import RecipeCard from "../components/RecipeCard";
 
 const AllRecipes = () => {
   const [recipes, setRecipes] = useState<RecipeCardType[]>([]);
@@ -21,10 +22,12 @@ const AllRecipes = () => {
 
   return (
     <div>
-      All Recipes
-      <div>
+      <h1 className="text-3xl font-bold text-center text-[var(--primary-color)] my-4">
+        All Recipes
+      </h1>
+      <div className="max-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8 mb-16 justify-items-center sm:justify-items-stretch">
         {recipes.map((recipe) => (
-          <div>{recipe.recipeName}</div>
+          <RecipeCard recipe={recipe} key={recipe._id} />
         ))}
       </div>
     </div>
